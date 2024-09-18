084cb826fbab74df6ac005d41ac360c331b9f629
Alex <kelheor23@gmail.com>
2024-09-18 12:18:53 +0300

    create base

---
+++ build/CMakeFiles/DockingPi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DockingPi.dir/main.cpp.o"
+  "CMakeFiles/DockingPi.dir/main.cpp.o.d"
+  "DockingPi"
+  "DockingPi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DockingPi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
