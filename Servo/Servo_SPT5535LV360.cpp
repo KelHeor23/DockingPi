252d368b8c319963d641787a3b34eaa36dcd02c1
Alex <kelheor23@gmail.com>
2024-09-19 15:39:03 +0300

    1

--- Servo/Servo_SPT5535LV360.cpp
+++ Servo/Servo_SPT5535LV360.cpp
@@ -4,8 +4,7 @@ Servo_SPT5535LV360::Servo_SPT5535LV360(int pin) : m_pin(pin) {
 }
 
 void Servo_SPT5535LV360::writePWM(PWM::SPT5535LV360 pwm){
-    softPwmCreate(m_pin, PWM::SPT5535LV360::STOP, PWM::SPT5535LV360::CV10); // Создание PWM на пине для управления сервоприводом
-    softPwmWrite(m_pin, pwm);
+    softPwmCreate(m_pin, pwm, 100); // Создание PWM на пине для управления сервоприводом
 }
 
 void Servo_SPT5535LV360::stop()
