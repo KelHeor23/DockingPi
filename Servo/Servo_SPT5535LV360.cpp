4773c1338056b65d3feea6ea81733d709f05e069
Alex <kelheor23@gmail.com>
2024-09-19 17:46:45 +0300

    add servo DS3235_270

--- Servo/Servo_SPT5535LV360.cpp
+++ Servo/Servo_SPT5535LV360.cpp
@@ -4,6 +4,6 @@ Servo_SPT5535LV360::Servo_SPT5535LV360(int pin) : m_pin(pin) {
     softPwmCreate(m_pin, 0, 100); // Создание PWM на пине для управления сервоприводом
 }
 
-void Servo_SPT5535LV360::writePWM(PWM::SPT5535LV360 pwm){
+void Servo_SPT5535LV360::writePWM(PWM pwm){
     softPwmWrite(m_pin, pwm) ;
 }
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 
 #include "Servo/Servo_SPT5535LV360.h"
+#include "Servo/Servo_DS3235_270.h"
 
 int main()
 {
@@ -12,16 +13,49 @@ int main()
         return 1;
     }
 
-    Servo_SPT5535LV360 servo1(7);
+    Servo_DS3235_270 servo1(7);
 
     while (true) {
-        servo1.writePWM(PWM::CV10);
+        servo1.writePWM(Servo_DS3235_270::CV1);
         usleep(500000); // Подождать 0.5 секунды
-
-        servo1.writePWM(PWM::CCV10);
+        servo1.writePWM(Servo_DS3235_270::CV2);
         usleep(500000); // Подождать 0.5 секунды
-
-        servo1.writePWM(PWM::STOP);
+        servo1.writePWM(Servo_DS3235_270::CV3);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CV4);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CV5);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CV6);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CV7);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CV8);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CV9);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CV10);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV1);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV2);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV3);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV4);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV5);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV6);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV7);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV8);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV9);
+        usleep(500000); // Подождать 0.5 секунды
+        servo1.writePWM(Servo_DS3235_270::CCV10);
+        usleep(500000); // Подождать 0.5 секунды
     }
 
     return 0 ;
