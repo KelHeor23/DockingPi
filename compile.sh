34a50098883d46db8115d54e1d16fdd7dbf66b8d
Alex <kelheor23@gmail.com>
2024-09-19 15:09:34 +0300

    Test servo 360

---
+++ compile.sh
@@ -0,0 +1,5 @@
+cmake -Bbuidl -H.
+cmake --build build -j4
+cd build
+make
+./DockingPi
--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <unistd.h>
+
+#include "Servo/Servo_SPT5535LV360.h"
 
 int main()
 {
+
     std::cout << "docking begin" << std::endl;
+
+    if (wiringPiSetup() == -1) {
+        std::cerr << "Ошибка инициализации WiringPi." << std::endl;
+        return 1;
+    }
+
+    Servo_SPT5535LV360 servo1(7);
+
+    while (true) {
+        servo1.writePWM(PWM::CV10);
+        usleep(1500000); // Подождать 0.5 секунды
+
+        servo1.writePWM(PWM::CCV10);
+        usleep(1500000); // Подождать 0.5 секунды
+
+        servo1.writePWM(PWM::STOP);
+        usleep(1500000); // Подождать 0.5 секунды
+    }
+
     return 0 ;
 }
