364aa0840af6530fb1d07440c0be97ec8577f3b8
Alex <kelheor23@gmail.com>
2024-09-20 18:15:00 +0300

    4

--- Mavlink/MavlinkExchange.cpp
+++ Mavlink/MavlinkExchange.cpp
@@ -3,24 +3,36 @@
 
 #include "MavlinkExchange.h"
 
-void MavlinkExchange::initMavlinkExchange(){
-    mavsdk = std::make_unique<Mavsdk>(Mavsdk::Configuration{Mavsdk::ComponentType::GroundStation});
+void MavlinkExchange::initMavlinkExchange()
+{
+    std::thread handleRcChannels(handle_rc_channels);
+    handleRcChannels.detach();
+}
 
-    ConnectionResult connection_result = mavsdk->add_any_connection("serial:///dev/serial0:57600");
+void MavlinkExchange::handle_rc_channels()
+{
+    Mavsdk mavsdk{Mavsdk::Configuration{Mavsdk::ComponentType::GroundStation}};
 
+    // ConnectionResult ret = mavsdk.add_udp_connection();
+    ConnectionResult connection_result = mavsdk.add_any_connection("serial:///dev/serial0:57600");
     if (connection_result != ConnectionResult::Success) {
         std::cout << "Adding connection failed: " << connection_result << '\n';
         return;
     }
 
-    while (mavsdk->systems().size() == 0){
+    while (mavsdk.systems().size() == 0){
         std::cout << "Systems wait connection" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+
     }
 
-    system = mavsdk->systems().at(0);
+    auto system = mavsdk.systems().at(0);
 
-    mavlink_passthrough = std::make_unique<MavlinkPassthrough>(system);
+    auto mavlink_passthrough = MavlinkPassthrough(system);
 
-    mavlink_passthrough->subscribe_message(MAVLINK_MSG_ID_RC_CHANNELS, [=](const mavlink_message_t& message){mavlink_msg_rc_channels_decode(&message, &rc_channels);});
+    // Подписываемся на сообщения MAVLink типа RC_CHANNELS
+    mavlink_passthrough.subscribe_message(MAVLINK_MSG_ID_RC_CHANNELS, [](const mavlink_message_t& message){mavlink_msg_rc_channels_decode(&message, &MavlinkExchange::rc_channels);});
+
+    while (true){
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 }
--- Mavlink/MavlinkExchange.h
+++ Mavlink/MavlinkExchange.h
@@ -6,16 +6,13 @@
 #include <mavsdk/plugins/telemetry/telemetry.h>
 #include <mavsdk/plugins/mavlink_passthrough/mavlink_passthrough.h>
 
+using namespace mavsdk;
+
 namespace MavlinkExchange {
-    using namespace mavsdk;
 
-    static std::unique_ptr<Mavsdk> mavsdk;
-    static std::shared_ptr<System> system;
-    static std::unique_ptr<MavlinkPassthrough> mavlink_passthrough;
-
+    void handle_rc_channels();
     static mavlink_rc_channels_t rc_channels;
 
-    void handle_rc_channels_message(const mavlink_message_t& message);
     void initMavlinkExchange();
 }
 
