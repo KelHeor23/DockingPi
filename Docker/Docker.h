ec98e6cc809e1af73c10d51b55a0cb02123e805c
Alex <kelheor23@gmail.com>
2024-09-24 16:21:40 +0300

    change pinout

--- Docker/Docker.h
+++ Docker/Docker.h
@@ -3,21 +3,14 @@
 
 #include "../Servo/Servo.h"
 
-constexpr uint8_t PIN_ROD           = 11;
-constexpr uint8_t PIN_CARGO         = 13;
-constexpr uint8_t PIN_RIGHT_HOOK    = 15;
-constexpr uint8_t PIN_LEFT_HOOK     = 19;
+#include "DockerPinout.h"
+
+
 
 class Docker {
 public:
     Docker() : servoRod(PIN_ROD), servoCargo(PIN_CARGO),
-        servoRightHook(PIN_RIGHT_HOOK), servoLeftHook(PIN_LEFT_HOOK) {
-
-        pinMode(PIN_ROD, OUTPUT);
-        pinMode(PIN_CARGO, OUTPUT);
-        pinMode(PIN_RIGHT_HOOK, OUTPUT);
-        pinMode(PIN_LEFT_HOOK, OUTPUT);
-    }
+        servoRightHook(PIN_RIGHT_HOOK), servoLeftHook(PIN_LEFT_HOOK) {}
 
     virtual ~Docker(){}
 
