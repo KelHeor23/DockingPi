370b6c145c5dc4bd353788c9e4d181a2051b3ecd
Alex <kelheor23@gmail.com>
2024-09-24 16:46:25 +0300

    stupid refactor mavlink exchange

---
+++ Mavlink/MavlinkExchange.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <thread>
+
+#include "MavlinkExchange.h"
+
+MavlinkExchange::MavlinkExchange() {}
+
+int MavlinkExchange::init()
+{
+    ConnectionResult connection_result = mavsdk->add_any_connection(serialAdress);
+    if (connection_result != ConnectionResult::Success) {
+        std::cout << "Adding connection failed: " << connection_result << '\n';
+        return 0;
+    }
+
+    while (mavsdk->systems().size() == 0){
+        std::cout << "Systems wait connection" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    auto system = mavsdk->systems().at(0);
+    mavlink_passthrough = std::make_unique<MavlinkPassthrough>(system);
+
+    // Подписываемся на сообщения MAVLink типа RC_CHANNELS
+    mavlink_passthrough->subscribe_message(MAVLINK_MSG_ID_RC_CHANNELS, [&](const mavlink_message_t& message){
+        mavlink_rc_channels_t rc_channels;
+        mavlink_msg_rc_channels_decode(&message, &rc_channels);
+        if (rc_channels.chancount >= 8) {
+            if (rc_channels.chan8_raw > 1600)
+                startPin = true;
+            else
+                startPin = false;
+        } else {
+            std::cout << "RC Channel 8 is not available." << std::endl;
+            return 0;
+        }
+        return 0;
+    });
+
+    return 1;
+}
+
+bool MavlinkExchange::getStartPin() const
+{
+    return startPin;
+}
---
+++ Mavlink/MavlinkExchange.h
@@ -0,0 +1,27 @@
+#ifndef MAVLINKEXCHANGE_H
+#define MAVLINKEXCHANGE_H
+
+#include <mavsdk.h>
+#include <mavsdk/system.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+#include <mavsdk/plugins/mavlink_passthrough/mavlink_passthrough.h>
+
+using namespace mavsdk;
+
+class MavlinkExchange
+{
+    std::unique_ptr<Mavsdk> mavsdk = std::make_unique<Mavsdk>(Mavsdk::Configuration{Mavsdk::ComponentType::GroundStation});
+    std::unique_ptr<MavlinkPassthrough> mavlink_passthrough;
+public:
+    MavlinkExchange();
+    int init();
+
+    bool getStartPin() const;
+
+private:
+    bool startPin = 0;
+
+    std::string serialAdress = "serial:///dev/serial0:115200";
+};
+
+#endif // MAVLINKEXCHANGE_H
