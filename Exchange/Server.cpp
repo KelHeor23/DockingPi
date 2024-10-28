3e6765d01fb9860c46f9519f3d3c65228e51f3a2
Alex <kelheor23@gmail.com>
2024-10-28 12:37:54 +0300

    add server

--- Exchange/Server.cpp
+++ Exchange/Server.cpp
@@ -1,18 +1,50 @@
 #include "Server.h"
 
-Server::Server(std::string address, std::size_t port) {}
+Server::Server(){
+}
 
 Server::~Server()
 {
-
+    close(clientSocket);
+    close(serverSocket);
 }
 
 void Server::sendMsg(std::string msg)
 {
-
+    send(clientSocket, msg.c_str(), msg.size(), 0);
 }
 
 std::string Server::read()
 {
+    std::string buffer;
+    buffer.resize(bufferSize);
+    ::read(clientSocket, buffer.data(), bufferSize);
+    return buffer;
+}
 
+bool Server::connect(std::string address, std::size_t port)
+{
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        throw("Error creating server socket");
+    }
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = inet_addr(address.c_str());
+
+    if ((bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr))) == 0){
+        throw("Error binding server socket to server adress");
+    }
+
+    std::size_t cntTrying = 5;
+    if ((listen(serverSocket, cntTrying)) < 0) {
+        throw("Error. " + std::to_string(cntTrying) + " linstening attempts is falls");
+    }
+
+    socklen_t addrlen = sizeof(serverAddr);
+    if ((clientSocket = accept(serverSocket, (struct sockaddr*)&serverAddr, (socklen_t*)&addrlen)) < 0){
+        throw("Error creating client socket in server");
+    }
+
+    return true;
 }
--- Exchange/Server.h
+++ Exchange/Server.h
@@ -7,15 +7,26 @@
 #define SERVER_H
 
 #include "IExchange.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
 class Server : public IExchange
 {
 public:
-    Server() = delete;
-    Server(std::string address, std::size_t port);
+    Server();
     ~Server();
 
-    void sendMsg(std::string msg)  override;
-    std::string read()          override;
+    void sendMsg(std::string msg)   override;
+    std::string read()              override;
+    bool connect(std::string address, std::size_t port) override;
+
+private:
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddr;
+    size_t bufferSize = 16;
 };
 
 #endif // SERVER_H
