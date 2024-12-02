9fa1c544bf0bc56ec777f5acbb31f4923f1fb101
Alex <kelheor23@gmail.com>
2024-12-02 11:17:52 +0300

    change balancing fo new cargo + 1.5 kg

--- config.h
+++ config.h
@@ -8,6 +8,6 @@ extern std::size_t port_g;
 
 
 /// Переменная определяющая расстоние на которое сдвинется телега для баланса выдвинутой стрелы
-constexpr int balance_cargo_g = 62;
+constexpr int balance_cargo_g = 45;
 
 #endif // CONFIG_H
