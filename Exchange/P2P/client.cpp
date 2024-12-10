eed3acb0059f4d1ffd5307c6ffc13ba914db2d21
Alex <kelheor23@gmail.com>
2024-12-10 07:59:50 +0300

    More error handlers

--- Exchange/P2P/client.cpp
+++ Exchange/P2P/client.cpp
@@ -15,7 +15,7 @@ void Client::exchange()
 {
     if (!checkConnection()){
         static bool conRefFirst = true;
-        boost::system::error_code error; // Переменная для хранения кода
+        boost::system::error_code error;
 
         socket.connect(tcp::endpoint(boost::asio::ip::address::from_string(address_g), port_g), error);
 
@@ -26,20 +26,31 @@ void Client::exchange()
             std::cout << "Connection complite!" << std::endl;
         }
     } else {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        if (socket.is_open()) {
+            boost::system::error_code write_error;
+            boost::asio::write(socket, boost::asio::buffer(readMsgPapa() + "\n"), write_error);
 
-        // Отправка сообщения
-        boost::asio::write(socket, boost::asio::buffer(readMsgPapa() + "\n"));
+            if (write_error) {
+                std::cerr << "Error when I try to send msg: " << write_error.message() << std::endl;
+                return;
+            }
 
-        // Принятие сообщения
-        boost::asio::streambuf buf;
-        boost::asio::read_until(socket, buf, '\n');
-        std::istream input_stream(&buf);
-        std::string message;
-        std::getline(input_stream, message);
+            boost::asio::streambuf buf;
+            boost::asio::read_until(socket, buf, '\n', write_error);
+            if (write_error) {
+                std::cerr << "Error when I try to read msg: " << write_error.message() << std::endl;
+                return;
+            }
 
-        writeMsgMama(message);
-        // Обработка принятого сообщения
-        std::cout << "Received: " << message << std::endl;
+            std::istream input_stream(&buf);
+            std::string message;
+            std::getline(input_stream, message);
+
+            writeMsgMama(message);
+            // Обработка принятого сообщения
+            std::cout << "Received: " << message << std::endl;
+        } else {
+            std::cerr << "Connection refuse. Try to connect..." << std::endl;
+        }
     }
 }
